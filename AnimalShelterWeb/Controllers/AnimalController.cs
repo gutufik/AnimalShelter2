0c95c5b8c4d8f5c9d87f0a704e9fdc8529d3e833
Михаил Мясников <gutufik@mail.ru>
2022-05-23 11:05:17 +0300

    started web project

---
+++ AnimalShelterWeb/Controllers/AnimalController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Core;
+
+namespace AnimalShelterWeb.Controllers
+{
+    public class AnimalController : Controller
+    {
+        public IActionResult Index()
+        {
+            var animals = DataAccess.GetAnimals();
+            return View(animals);
+        }
+        public IActionResult Edit(int id)
+        {
+            var animal = DataAccess.GetAnimal(id);
+            return View(animal);
+        }
+        [HttpPost]
+        public IActionResult Edit(Animal animal)
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ AnimalShelterWeb/Controllers/MedicineController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Core;
+
+namespace AnimalShelterWeb.Controllers
+{
+    public class MedicineController : Controller
+    {
+        public IActionResult Index()
+        {
+            var medicines = DataAccess.GetMedicines();
+            return View(medicines);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
