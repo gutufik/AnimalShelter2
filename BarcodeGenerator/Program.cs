a5214ae1c456c7266112e82aeaf0dcb6f505cd2e
Михаил Мясников <gutufik@mail.ru>
2022-11-22 11:27:37 +0300

    Add animals images

---
+++ BarcodeGenerator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using IronBarCode;
+
+
+namespace BarcodeGenerator
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            GeneratedBarcode barcode = IronBarCode.BarcodeWriter.CreateBarcode("https://www.youtube.com/", BarcodeEncoding.QRCode);
+            barcode.SaveAsPng("barcode.png");
+        }
+    }
+}
